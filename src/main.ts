b2ed93f0d7263c37f9aac8df574bfaf4243bf47b
ROHIT KUMAR <mailrohitkr6@gmail.com>
2022-01-30 12:34:59 +0530

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,65 @@
+//import './style.css'
+import datas from "./data.json";
+var data = {
+  nodes: [
+    { id: "Richard" },
+    { id: "Larry" },
+    { id: "Marta" },
+    { id: "Jane" },
+    { id: "Norma" },
+    { id: "Frank" },
+    { id: "Brett" },
+  ],
+  edges: [
+    { from: "Richard", to: "Larry" },
+    { from: "Richard", to: "Marta" },
+    { from: "Larry", to: "Marta" },
+    { from: "Marta", to: "Jane" },
+    { from: "Jane", to: "Norma" },
+    { from: "Jane", to: "Frank" },
+    { from: "Jane", to: "Brett" },
+    { from: "Brett", to: "Frank" },
+  ],
+};
+declare const anychart: any;
+const drawGraph = function (datas) {
+  // create an instance of a pie chart
+  document.getElementById("container").style.display = "block";
+  var chart = anychart.graph(datas);
+  // set the data
+  chart.nodes().labels().format("{%id}");
+  chart.nodes().labels().fontSize(12);
+  chart.interactivity().zoomOnMouseWheel(false);
+  chart.nodes().labels().enabled(true);
+  chart.fit();
+  var zoomController = anychart.ui.zoom();
+  zoomController.target(chart);
+  zoomController.render();
+  chart.move(50, -100);
+  // set chart title
+  chart.title("Dependency Graph");
+  // set the container element
+  chart.container("container");
+  // initiate chart display
+  //chart.draw();
+  chart.draw();
+};
+document.getElementById("import").onclick = function () {
+  var files = (document.getElementById("selectFiles") as any).files;
+  console.log(files);
+  if (files.length <= 0) {
+    return false;
+  }
+
+  var fr = new FileReader();
+
+  fr.onload = function (e: any) {
+    console.log(e);
+    var result = JSON.parse(e.target.result);
+
+    drawGraph(result);
+    //(document.getElementById("result") as any).value = formatted;
+  };
+
+  fr.readAsText(files.item(0));
+};
